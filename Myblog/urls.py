8a5acaa3f16d3cf3d4f2d4da2bc112c9ec9055ec
HANZIGUO <hanziguo@kingsoft.com>
2019-08-27 14:52:46 +0800

    'article_list'

--- Myblog/urls.py
+++ Myblog/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'Myblog'
 urlpatterns = [
     path('index/', views.index, name='index'),
+    path('article/<int:article_id>', views.article_detail, name='article_detail'),
+    path('article/', views.article_list, name='article_list'),
 ]
--- Myblog/views/myblog_views.py
+++ Myblog/views/myblog_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, render_to_response, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from ..models import ArticleModels
 from django.template import loader
@@ -9,11 +9,28 @@ from django.urls import reverse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!!")
-    # latest_question_list = ArticleModels.objects.order_by('-pub_date')[:5]
-    # context = {
-    #     'latest_question_list': latest_question_list,
-    # }
-    # return render(request, 'Myblog/index.html', context)
 
+
+def article_detail(request, article_id):
+    # try:
+    #     article = ArticleModels.Article.objects.get(pk=article_id)
+    #     #return HttpResponse("<h2>文章标题: %s</h2> <br> 文章内容: %s" % (article.title, article.content))
+    #     context = {}
+    #     context['article'] = article
+    #     return render(request, "Myblog/article_detail.html", context)
+    # except ArticleModels.Article.DoesNotExist:
+    #     raise Http404("id not exist")
+
+    article = get_object_or_404(ArticleModels.Article, pk=article_id)
+    context = {}
+    context['article'] = article
+    return render_to_response("Myblog/article_detail.html", context)
+
+
+def article_list(request):
+    articles = ArticleModels.Article.objects.all()
+    context = {}
+    context['articles'] = articles
+    return render_to_response("Myblog/article_list.html", context)
 
 
