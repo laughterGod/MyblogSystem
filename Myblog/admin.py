556f98f541fb8ee14ebf69b1182f225d6f637c74
HANZIGUO <hanziguo@kingsoft.com>
2019-08-27 16:02:35 +0800

    'fixblog'

--- Myblog/admin.py
+++ Myblog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Article
 
 # Register your models here.
-admin.site.register(Article)
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'content', 'author' ,'ctime', 'utime')
+    ordering = ('-id',)
+
+
+#admin.site.register(Article, ArticleAdmin)
--- Myblog/models/ArticleModels.py
+++ Myblog/models/ArticleModels.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    # ctime = models.DateTimeField(default=timezone.now())
+    ctime = models.DateTimeField(auto_now_add=True)
+    utime = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, default=1)
+
+    # 废弃了，已经在admin.py定制了
+    def __str__(self):
+        return self.title
 
