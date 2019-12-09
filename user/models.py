5aabe44415eda220855d2dc66f68922a947b1d33
hanziguo <hanziguo@kingsoft.com>
2019-12-09 11:18:21 +0800

    user

---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='用户名')
+    nickname = models.CharField(max_length=20, verbose_name='昵称')
+
+    def __str__(self):
+        return '<Profile：%s for %s>' % (self.nickname, self.user.username)
+
