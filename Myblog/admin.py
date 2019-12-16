3982bf8d6bc053d29c1ac66fcff2c07fb41a860b
hanziguo <hanziguo@kingsoft.com>
2019-12-16 11:19:15 +0800

    china

--- Myblog/admin.py
+++ Myblog/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import Article, Blog, BlogType
 
 # Register your models here.
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'content', 'author' ,'ctime', 'utime', 'is_deleted')
-    ordering = ('-id',)
+# @admin.register(Article)
+# class ArticleAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'title', 'content', 'author' ,'ctime', 'utime', 'is_deleted')
+#     ordering = ('-id',)
 
 
 @admin.register(BlogType)
