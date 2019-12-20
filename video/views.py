f49aa8842e8f85e97e218edd41b827aa172340ae
hanziguo <hanziguo@kingsoft.com>
2019-12-20 12:59:30 +0800

    video

--- video/views.py
+++ video/views.py
@@ -1,14 +1,7 @@
-import os
-import sys
 from django.shortcuts import render
 from django.views import generic
 
 from video.models import Video
-
-home_dir = os.path.join(os.path.split(os.path.realpath(__file__))[0], os.path.pardir)
-media_dir = home_dir + '/media/'
-
-sys.path.append(media_dir)
 
 
 class IndexView(generic.ListView):
@@ -23,7 +16,6 @@ class VideoDetailView(generic.DetailView):
 
     def get_object(self, queryset=None):
         obj = super().get_object()
-        os.system("chmod -R 777 " + media_dir)
         obj.increase_view_count()  # 调用自增函数
         return obj
 
